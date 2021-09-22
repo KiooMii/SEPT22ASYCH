7307aaed08e7c4f6aedd2531f3249e89365cb4dd
KiooMii <kio.mingote@gmail.com>
2021-09-22 14:34:22 +0800

    Update Problem5.java

--- src/main/java/Problem5.java
+++ src/main/java/Problem5.java
@@ -4,26 +4,36 @@ public class Problem5 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
 		
+        
+        
 		System.out.println("Enter the area of the shape you want to compute for (square, triangle, rectangle)");
 		String shape = in.nextLine();
+                boolean inc = false;
+                
+               while(inc==false){
+                   
                 
                 if(shape.equals("square")){
 		
         System.out.println("Enter the the measurement of the side of the square");
-        int measurement = in.nextInt();
+        char measurement = in.next().charAt(0);
         long comp = measurement * measurement;
         System.out.println("The area of the square is " + comp);
+        inc = true;
+        System.exit(0);
                 
                 }
                 
                 if (shape.equals("rectangle")){
 		
         System.out.println("Enter the the height of the rectangle");
-        int measurement1 = in.nextInt();
+        byte measurement1 = in.nextByte();
         System.out.println("Enter the the width of the rectangle");
-        int measurement2 = in.nextInt();
+        short measurement2 = in.nextShort();
         int comp = measurement1 * measurement2;
         System.out.println("The area of the rectangle is " + comp);
+        inc = true;
+        System.exit(0);
                 
                 }
                 
@@ -35,8 +45,19 @@ public class Problem5 {
         float measurement4 = in.nextFloat();
         double comp = (measurement4 / 2) * measurement3;
         System.out.println("The area of the triangle " + comp);
+        inc = true;
+        System.exit(0);
                 
                 }
+                
+                else{
+                    
+                    System.out.println("Wrong Input.Enter the area of the shape you want to compute for (square, triangle, rectangle)");
+                    shape = in.nextLine();
+                
+                }
+                
+               }
                 
                 
                    
